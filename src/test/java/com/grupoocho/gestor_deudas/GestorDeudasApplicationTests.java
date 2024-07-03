5c0a55df1f245a910f9491ff1656a7ce81064a2c
villitaqt <diegovillaju11@gmail.com>
2024-07-03 01:10:00 -0500

    Initial commit: Project setup with Spring Boot

---
+++ src/test/java/com/grupoocho/gestor_deudas/GestorDeudasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoocho.gestor_deudas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeudasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
