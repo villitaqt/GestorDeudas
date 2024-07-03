164aee7b991e2929e2b78d998abedebf247b15e4
villitaqt <diegovillaju11@gmail.com>
2024-07-03 01:10:00 -0500

    Initial commit: Project setup with Spring Boot

---
+++ src/main/java/com/grupoocho/gestor_deudas/GestorDeudasApplication.java
@@ -0,0 +1,13 @@
+package com.grupoocho.gestor_deudas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeudasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeudasApplication.class, args);
+	}
+
+}
